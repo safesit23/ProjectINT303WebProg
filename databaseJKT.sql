fa16e1a7688d4e0cdd0faae354105922dd6cbdb2
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-13 14:09:10 +0700

    [UPDATE] edit sql fro create table

--- databaseJKT.sql
+++ databaseJKT.sql
@@ -23,11 +23,11 @@ CREATE TABLE account (
 ) ;
 
 /*Data for table account */
-insert into account(username,password,firstName,lastName,nickname,sex,phone) values 
-('test@test.com','test','FirstName','LastName','Admin','M','0123456789');
+insert into account(username,password,firstName,lastName,nickname,sex,phone,activatekey) values 
+('test@test.com','test','FirstName','LastName','Admin','M','0123456789','activatekey');
 
 
-/*********************Table structure for table product *********************/
+/*********************Table structure for table shoe *********************/
 DROP TABLE  shoe;
 
 CREATE TABLE shoe (
@@ -35,116 +35,83 @@ CREATE TABLE shoe (
   shoeName varchar(70) NOT NULL,
   color varchar(15) NOT NULL,
   shoeType varchar(20) NOT NULL,
+  shoeType2 varchar(10) check (shoeType2 in ('MEN','WOMEN','ALL')) NOT NULL,
   brand varchar(20) NOT NULL,
   price int NOT NULL,
 
-  CONSTRAINT shoe_shoeId_pk PRIMARY KEY (shoeId),
+  CONSTRAINT shoe_shoeId_pk PRIMARY KEY (shoeId)
 ) ;
+
+/*Data for the table shoe */
+insert  into shoe(shoeId,shoeName,color,shoeType,shoeType2,brand,price) values
+('NI001SH200','NikeAir','black','running','MEN','nike',3000);
+
+
+/********************Table structure for table sizestock ********************/
+DROP TABLE sizeSpecific;
+
+CREATE table sizeSpecific(
+  shoeId varchar(10) NOT NULL,
+  shoeSize int NOT NULL,
+  CONSTRAINT sizeSpecific_sIdSize_pk PRIMARY KEY (shoeId,shoeSize)
+);
 
 /*Data for the table product */
-insert  into product(shoeId,shoeName,color,shoeType,brand,price) values 
-('NI001SH200','NikeAir','black','running','nike',3000),
+insert into sizeSpecific(shoeId,shoeSize) values
+('NI001SH200',7),
+('NI001SH200',9),
+('NI001SH200',10);
 
-
-
-
-/********************Table structure for table payment ********************/
-
-DROP TABLE payment;
-
-CREATE TABLE payment (
-  paymentId int NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1001, INCREMENT BY 1),
-  paymentDate date NOT NULL,
-  paymentMethod varchar(20) NOT NULL,
-  paymentAmount decimal(10,2) NOT NULL,
-
-  CONSTRAINT payment_paymentId_pk PRIMARY KEY (paymentId)
-) ;
-
-/*Data for the table payment */
-insert into payment(paymentDate,paymentMethod,paymentDate,amount) values 
-('2018-10-14','credit card',5000.50);
-
-
-/*******************Table structure for table order *****************/
+/*******************Table structure for table orders *****************/
 DROP TABLE orders;
 
 CREATE TABLE orders (
   orderId int NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 6013001, INCREMENT BY 1),
   orderDate date NOT NULL,
-  shipTo address varchar(250) NOT NULL,
-  requiredDate date NOT NULL,
-  shippedDate date DEFAULT NULL,
-  status varchar(15) NOT NULL,
-  comments clob,
+  shipTo varchar(250) NOT NULL,
+  shippedDate date NOT NULL,
+  totalPrice decimal(10,2) NOT NULL,
   accountId int NOT NULL,
-  CONSTRAINT orders_orderId_pk PRIMARY KEY (orderId),
-  CONSTRAINT orders_accountId_fk FOREIGN KEY (accountId) REFERENCES account (accountId)
+  CONSTRAINT order_orderId_pk PRIMARY KEY (orderId),
+  CONSTRAINT order_accountId_fk FOREIGN KEY (accountId) REFERENCES account (accountId)
 ) ;
 
 /*Data for the table orders */
-
-insert  into orders(orderNumber,orderDate,requiredDate,shippedDate,status,comments,customerNumber) values 
-(10100,'2003-01-06','2003-01-13','2003-01-10','Shipped',NULL,363),
-(10101,'2003-01-09','2003-01-18','2003-01-11','Shipped','Check on availability.',128);
+insert into orders(orderDate,shipTo,shippedDate, totalPrice,accountId) values
+('2018-11-01','BangkokTH','2018-11-08',1000,1);
 
 
+/*********************Table structure for table orderDetail *********************/
+DROP TABLE orderDetail;
 
-
-
-
-
-/*Table structure for table productline */
-
-DROP TABLE productline;
-
-CREATE TABLE productline (
-  productLine varchar(50) NOT NULL,
-  textDescription varchar(4000) DEFAULT NULL,
-  htmlDescription clob,
-  image blob,
-  PRIMARY KEY (productLine)
-) ;
-
-/*Data for the table productlines */
-
-insert  into productline(productLine,textDescription,htmlDescription,image) values 
-('Classic Cars','Attention car enthusiasoffice.',NULL,NULL),
-('Vintage Cars','Our Vintage Car models realistically portray automobile office.',NULL,NULL);
-
-/*Table structure for table orderdetails */
-
-DROP TABLE orderdetail;
-
-CREATE TABLE orderdetail (
-  orderNumber int NOT NULL,
-  productCode varchar(15) NOT NULL,
-  quantityOrdered int NOT NULL,
-  priceEach decimal(10,2) NOT NULL,
-  orderLineNumber smallint NOT NULL,
-  PRIMARY KEY (orderNumber,productCode),
-  CONSTRAINT orderdetails_ibfk_1 FOREIGN KEY (orderNumber) REFERENCES orders (orderNumber),
-  CONSTRAINT orderdetails_ibfk_2 FOREIGN KEY (productCode) REFERENCES product (productCode)
-) ;
-
-/*Data for the table orderdetails */
-
-insert  into orderdetail (orderNumber,productCode,quantityOrdered,priceEach,orderLineNumber) values 
-(10100,'S18_1749',30,136.00,3);
-
-
-
-/********************Table structure for table sizestock ********************/
-DROP TABLE sizespecific;
-CREATE table sizespecific(
+CREATE TABLE orderDetail (
+  orderId int NOT NULL,
   shoeId varchar(10) NOT NULL,
   shoeSize int NOT NULL,
-  CONSTRAINT stocksize_sIdSize_pk PRIMARY KEY (shoeId,shoeSize)
+  quantity int NOT NULL,
+  price decimal(10,2) NOT NULL,
+  CONSTRAINT orderdetail_orderId_pk PRIMARY KEY (orderId,shoeId,shoeSize),
+  CONSTRAINT orderdetail_orderId_fk1 FOREIGN KEY (orderId) REFERENCES orders (orderId),
+  CONSTRAINT orderdetail_shoeId_fk2 FOREIGN KEY (shoeId) REFERENCES shoe (shoeId)
+) ;
+
+/*Data for the table orderDetail */
+insert  into orderDetail (orderId,shoeId,shoeSize,quantity,price) values
+(6013001,'NI001SH200',9,1,3000);
+
+/*********************Table structure for table voucher *********************/
+DROP TABLE voucher;
+
+CREATE TABLE voucher (
+  voucherId varchar(10) NOT NULL,
+  voucherValue int NOT NULL,
+  minSpend int DEFAULT 0,
+  validate varchar(5) check (validate in ('true','false')) DEFAULT 'true',
+  comment varchar(40) DEFAULT NULL,
+
+  CONSTRAINT voucher_vId_pk PRIMARY KEY (voucherId)
 );
 
-
-
-
-
-
+insert  into voucher(voucherId,voucherValue,minSpend) values
+('NEW100',100,1500);
 /*end of statements */
