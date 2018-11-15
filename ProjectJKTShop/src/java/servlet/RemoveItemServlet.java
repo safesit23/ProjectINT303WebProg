6ec1482320fe89684868b29481b3dcf5fe8e471b
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-15 23:03:54 +0700

    [UPDATE] remove product servlet

--- ProjectJKTShop/src/java/servlet/RemoveItemServlet.java
+++ ProjectJKTShop/src/java/servlet/RemoveItemServlet.java
@@ -5,18 +5,33 @@
  */
 package servlet;
 
+import controller.ShoeJpaController;
+import controller.SizeSpecificJpaController;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+import model.Cart;
+import model.Shoe;
+import model.SizeSpecific;
 
 /**
  *
  * @author jatawatsafe
  */
 public class RemoveItemServlet extends HttpServlet {
+
+    @PersistenceUnit(unitName = "JKTShopPU")
+    EntityManagerFactory emf;
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,7 +44,26 @@ public class RemoveItemServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        HttpSession session = request.getSession(false);
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (cart != null) {
+            String shoeId = request.getParameter("shoeId");
+            String shoeSizeText = request.getParameter("shoeSize");
+            if (shoeId != null && shoeSizeText != null) {
+                int shoeSize = Integer.parseInt(shoeSizeText);
+                ShoeJpaController sCtrl = new ShoeJpaController(utx, emf);
+                SizeSpecificJpaController ssCtrl = new SizeSpecificJpaController(utx, emf);
+                Shoe shoe = sCtrl.findShoe(shoeId);
+                SizeSpecific ss = new SizeSpecific(shoeId, shoeSize);
+                cart.remove(ss.getSizeSpecificPK().getSpecificShoe());
+            }
+        }
+        String page = request.getParameter("page");
+        if (page != null) {
+            response.sendRedirect("Cart.jsp");
+        } else {
+            response.sendRedirect("AboutProductServlet");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
