4517326e587110f56e5d7ec79b3271700ee001f8
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-15 20:01:06 +0700

    [UPDATE] add Cart.java

--- ProjectJKTShop/src/java/model/Cart.java
+++ ProjectJKTShop/src/java/model/Cart.java
@@ -5,10 +5,50 @@
  */
 package model;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author jatawatsafe
  */
 public class Cart {
+    private Map<String, CartDetail> cart;
+    //Key = String SpecificShoe
+    //Value = CartDetail(SizeSpecific ss)
+
+    public Cart() {
+        cart = new HashMap();
+    }
+    
+    public void add(SizeSpecific ss){
+        CartDetail line = cart.get(ss.getSizeSpecificPK().getSpecificShoe());
+        //if don't have this shoe
+        if(line==null){
+            cart.put(ss.getSizeSpecificPK().getSpecificShoe(), new CartDetail(ss));
+        }else{
+            line.setQuantity(line.getQuantity()+1);
+        }
+    }
+    
+    public void remove(SizeSpecific ss){
+        CartDetail line = cart.get(ss.getSizeSpecificPK().getSpecificShoe());
+        if(line.getQuantity()==1){
+            cart.remove(ss.getSizeSpecificPK().getSpecificShoe());
+        }else{
+            line.setQuantity(line.getQuantity()-1);
+        }
+    }
+    
+    public int getTotalQuantity(){
+        int sum = 0;
+        Collection<CartDetail> details = cart.values();
+        for (CartDetail item : details) {
+            sum+=item.getQuantity();
+        }
+        return sum;
+    }
+    
     
 }
