43ff7ec100a694e58c8673a70ae85ebd2dec85ad
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-16 00:47:36 +0700

    [UPDATE] send to Message.jsp

--- ProjectJKTShop/src/java/servlet/RegisterServlet.java
+++ ProjectJKTShop/src/java/servlet/RegisterServlet.java
@@ -29,7 +29,7 @@ public class RegisterServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
