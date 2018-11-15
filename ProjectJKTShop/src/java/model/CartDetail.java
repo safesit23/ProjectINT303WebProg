9163953c46145d67a08deffd9ed392d503c09084
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-15 18:56:34 +0700

    [ADD] CartDetail

---
+++ ProjectJKTShop/src/java/model/CartDetail.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class CartDetail {
+    Shoe shoe;
+    int shoeSize;
+    int quantity;
+    int price;
+
+    public CartDetail() {
+    }
+
+    public CartDetail(Shoe shoe, int shoeSize, int quantity) {
+        this.shoe = shoe;
+        this.shoeSize = shoeSize;
+        this.quantity = quantity;
+        this.price = shoe.getPrice();
+    }
+
+    public Shoe getShoe() {
+        return shoe;
+    }
+
+    public void setShoe(Shoe shoe) {
+        this.shoe = shoe;
+    }
+
+    public int getShoeSize() {
+        return shoeSize;
+    }
+
+    public void setShoeSize(int shoeSize) {
+        this.shoeSize = shoeSize;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return shoe.getPrice();
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    public double getTotalPrice(){
+        return this.price*this.quantity;
+    }
+}
