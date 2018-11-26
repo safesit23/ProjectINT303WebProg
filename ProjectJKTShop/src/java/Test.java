69845da309d9182b15a70876113acbc8b6009fb0
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-11-26 16:07:51 +0700

    [EDIT] test orders

--- ProjectJKTShop/src/java/Test.java
+++ ProjectJKTShop/src/java/Test.java
@@ -1,11 +1,9 @@
-
-import java.time.LocalDate;
 import java.util.Date;
 import model.Orders;
 
 public class Test {
     public static void main(String[] args) {
-        Orders orders = new Orders(new Date(),"This place",7,2000);
+        Orders orders = new Orders(new Date(),"This place",7,2000,null);
         System.out.println("----------\n\nOrders="+orders);
 	  
     }
